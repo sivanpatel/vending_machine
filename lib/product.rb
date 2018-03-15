4b883cedc55123f9284b450f80f52bcd658a67e3
Sivan Patel <sivan.patel@googlemail.com>
2018-03-15 16:14:06 +0000

    product has a price attribute

--- lib/product.rb
+++ lib/product.rb
@@ -1,11 +1,16 @@
 class Product
 
-  def initialize(name:)
+  def initialize(name:, price:)
     @name = name
+    @price = price
   end
 
   def name
     @name
+  end
+
+  def price
+    @price
   end
 
 end
--- spec/product_spec.rb
+++ spec/product_spec.rb
@@ -2,9 +2,13 @@ require 'product'
 
 describe Product do
 
-  let(:lucozade) { described_class.new(name: 'Lucozade') }
+  let(:lucozade) { described_class.new(name: 'Lucozade', price: 1.00) }
 
   it 'has a name' do
     expect(lucozade.name).to eq 'Lucozade'
+  end
+
+  it 'has a price' do
+    expect(lucozade.price).to eq 1.00
   end
 end
