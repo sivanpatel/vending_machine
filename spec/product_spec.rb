f092f5f3c25390ed4dd5e86e7703f201a6fa12a5
Sivan Patel <sivan.patel@googlemail.com>
2018-03-15 16:36:48 +0000

    price should be in pence not pounds

--- spec/product_spec.rb
+++ spec/product_spec.rb
@@ -2,14 +2,14 @@ require 'product'
 
 describe Product do
 
-  let(:lucozade) { described_class.new(name: 'Lucozade', price: 1.00, quantity: 10) }
+  let(:lucozade) { described_class.new(name: 'Lucozade', price: 100, quantity: 10) }
 
   it 'has a name' do
     expect(lucozade.name).to eq 'Lucozade'
   end
 
   it 'has a price' do
-    expect(lucozade.price).to eq 1.00
+    expect(lucozade.price).to eq 100
   end
 
   it 'has an initial quantity' do
@@ -17,7 +17,7 @@ describe Product do
   end
 
   it 'can be initialized with a default quantity of 5' do
-    sandwich = Product.new(name: 'Sandwich', price: 1.00)
+    sandwich = Product.new(name: 'Sandwich', price: 100)
     expect(sandwich.quantity).to eq 5
   end
 
