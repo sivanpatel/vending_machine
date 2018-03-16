4fc376972191c4ad4c787fbe2f984d3d99e0eac1
Sivan Patel <sivan.patel@googlemail.com>
2018-03-16 17:11:31 +0000

    can create table of products, and can enter coins and keep track of them (vending machine)

--- lib/vending_machine.rb
+++ lib/vending_machine.rb
@@ -1,11 +1,15 @@
+require 'terminal-table'
 require_relative './stock'
 require_relative './money/bank'
 
 class VendingMachine
 
+  VALID_COIN_DENOMINATIONS = [200, 100, 50, 20, 10, 5, 2, 1]
+
   def initialize
     @stock = Stock.new
     @bank = Money::Bank.new
+    @coins = {}
   end
 
   def stock
@@ -15,4 +19,27 @@ class VendingMachine
   def bank
     @bank.coins
   end
+
+  def coins_entered
+    @coins
+  end
+
+  def print_stock
+    table = Terminal::Table.new :headings => ['Item Number', 'Item Name', 'Item Price', 'Remaining Stock'], :rows => table_rows
+  end
+
+  def enter_coin(coin_value)
+    @coins[coin_value].nil? ? @coins[coin_value] = 1 : @coins[coin_value] += 1
+  end
+
+  private
+
+  def table_rows
+    rows = []
+    stock.each_with_index do |product, index|
+      rows << [(index + 1), product.name, product.price, product.quantity]
+    end
+    rows
+  end
 end
+
--- spec/vending_machine_spec.rb
+++ spec/vending_machine_spec.rb
@@ -8,5 +8,20 @@ describe VendingMachine do
     expect(vending_machine.stock.length).to eq 5
     expect(vending_machine.bank.length).to eq 8
   end
+
+  it 'starts with initially empty coins' do
+    expect(vending_machine.coins_entered).to eq({})
+  end
+
+  it 'creates a table of the stocklist' do
+    expect(vending_machine.print_stock).to be_a Terminal::Table
+  end
+
+  it 'keeps a track of the coins entered' do
+    vending_machine.enter_coin(20)
+    vending_machine.enter_coin(20)
+    vending_machine.enter_coin(5)
+    expect(vending_machine.coins_entered).to eq({20=>2,5=>1})
+  end
 end
 
