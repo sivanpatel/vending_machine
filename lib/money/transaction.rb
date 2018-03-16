5cfd5074abd41d28b904a003a625d9d488286503
Sivan Patel <sivan.patel@googlemail.com>
2018-03-16 13:54:28 +0000

    transaction returning the number of coins needed

--- lib/money/transaction.rb
+++ lib/money/transaction.rb
@@ -1,6 +1,8 @@
 module Money
   class Transaction
 
+    VALID_COIN_DENOMINATIONS = [200, 100, 50, 20, 10, 5, 2, 1]
+
     def initialize(product_value:, money_given:)
       @product_value = product_value
       @money_given = money_given
@@ -8,6 +10,16 @@ module Money
 
     def difference
       @money_given - @product_value
+    end
+
+    def change
+      change_due = difference
+      coins_due = {}
+      VALID_COIN_DENOMINATIONS.each do |coin_value|
+        number_of_coins, change_due = change_due.divmod(coin_value)
+        coins_due[coin_value] = number_of_coins
+      end
+      coins_due
     end
   end
 end
--- spec/money/transaction_spec.rb
+++ spec/money/transaction_spec.rb
@@ -3,9 +3,19 @@ require 'money/transaction'
 describe Money::Transaction do
 
   it 'calculates the difference between the money given and product_value' do
-    product_value = 123
-    money_given = 125
-    transaction = described_class.new(product_value: product_value, money_given: money_given)
+    transaction = described_class.new(product_value: 123, money_given: 125)
     expect(transaction.difference).to eq 2
+  end
+
+  describe '#change' do
+    it 'returns a list of coins needed for change' do
+      transaction = described_class.new(product_value: 100, money_given: 124)
+      expect(transaction.change).to eq ({1=>0, 2=>2, 5=>0, 10=>0, 20=>1, 50=>0, 100=>0, 200=>0})
+    end
+
+    it 'returns a list of coins needed for change' do
+      transaction = described_class.new(product_value: 45, money_given: 124)
+      expect(transaction.change).to eq ({1=>0, 2=>2, 5=>1, 10=>0, 20=>1, 50=>1, 100=>0, 200=>0})
+    end
   end
 end
