38a7c41719e4eebfce7f2bffe23fff3da4cc7ec6
Sivan Patel <sivan.patel@googlemail.com>
2018-03-16 10:52:49 +0000

    change namespace of coin to be under money module

---
+++ lib/money/coin.rb
@@ -0,0 +1,25 @@
+module Money
+  class Coin
+
+    def initialize(value:, quantity: 10)
+      @value = value
+      @quantity = quantity
+    end
+
+    def value
+      @value
+    end
+
+    def quantity
+      @quantity
+    end
+
+    def restock(amount)
+      @quantity += amount
+    end
+
+    def release(amount)
+      @quantity -= amount
+    end
+  end
+end
---
+++ spec/money/coin_spec.rb
@@ -0,0 +1,29 @@
+require 'coin'
+
+describe Money::Coin do
+
+  let(:twenty) { described_class.new(value: 20, quantity: 5) }
+
+  it 'has a value' do
+    expect(twenty.value).to eq 20
+  end
+
+  it 'has a specified starting quantity' do
+    expect(twenty.quantity).to eq 5
+  end
+
+  it 'has a default starting quantity if not otherwise specified' do
+    coin = described_class.new(value: 10)
+    expect(coin.quantity).to eq 10
+  end
+
+  it 'can restock coins by a certain amount' do
+    twenty.restock(5)
+    expect(twenty.quantity).to eq 10
+  end
+
+  it 'can release an amount of coins' do
+    twenty.release(3)
+    expect(twenty.quantity).to eq 2
+  end
+end
