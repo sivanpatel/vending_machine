af62ed5b8e4dd4aee281ed4b18848e724abf76fc
Sivan Patel <sivan.patel@googlemail.com>
2018-03-17 10:33:58 +0000

    moved stocklist to private generate method

--- lib/stock.rb
+++ lib/stock.rb
@@ -3,11 +3,7 @@ require_relative './product'
 class Stock
 
   def initialize
-    @stock = [Product.new(name: 'Espresso', price: 37),
-              Product.new(name: 'Capuccino', price: 43),
-              Product.new(name: 'Latte', price: 29),
-              Product.new(name: 'Americano', price: 15),
-              Product.new(name: 'Flat White', price: 50)]
+    @stock = generate_stocklist
   end
 
   def stocklist
@@ -22,5 +18,15 @@ class Stock
   def release_product(product_name)
     product = stocklist.find { |product| product.name == product_name }
     product.release
+  end
+
+  private
+
+  def generate_stocklist
+    [Product.new(name: 'Espresso', price: 37),
+     Product.new(name: 'Capuccino', price: 43),
+     Product.new(name: 'Latte', price: 29),
+     Product.new(name: 'Americano', price: 15),
+     Product.new(name: 'Flat White', price: 50)]
   end
 end
