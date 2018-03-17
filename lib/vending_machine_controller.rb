949c168f1a7fc7e1306d33383db8b2032f9c7953
Sivan Patel <sivan.patel@googlemail.com>
2018-03-17 16:52:38 +0000

    mostly working now, need to raise errors in a more graceful way instead of breaking, and return change

--- lib/vending_machine_controller.rb
+++ lib/vending_machine_controller.rb
@@ -6,12 +6,20 @@ class VendingMachineController
     @vending_machine = VendingMachine.new
   end
 
+  def start_vending_machine
+    loop do
+      puts vending_machine.print_stock
+      control_flow
+    end
+  end
+
   def vending_machine
     @vending_machine
   end
 
-  def control_flow(input)
-    input = input.strip.downcase
+  def control_flow
+    puts 'inital control flow'
+    input = STDIN.gets.chomp
     return unless input == 'restock' || input == 'buy' || input == 'exit'
     restock_control_flow if input == 'restock'
     vending_control_flow if input == 'buy'
@@ -19,10 +27,50 @@ class VendingMachineController
   end
 
   def restock_control_flow
-
+    puts 'restock control flow'
+    input = STDIN.gets.chomp
+    input = input.strip.downcase
+    return unless input == 'products' || input == 'coins'
+    products_restock_control_flow if input == 'products'
+    coins_restock_control_flow if input == 'coins'
   end
 
   def vending_control_flow
+    puts 'vending control flow'
+    enter_coins
+    puts 'now time to buy'
+    product_code = STDIN.gets.chomp.to_i
+    vending_machine.choose_product(product_code)
+    vending_machine.vend_item
+    return
+  end
 
+  def products_restock_control_flow
+    puts 'prod restock control flow'
+    product_code = STDIN.gets.chomp.to_i - 1
+    amount_to_restock = STDIN.gets.chomp.to_i
+    vending_machine.stock.stocklist[product_code].restock(amount_to_restock)
+    return
+  end
+
+  def coins_restock_control_flow
+    puts 'coin restock control flow'
+    coin_value = STDIN.gets.chomp.to_i
+    amount_to_restock = STDIN.gets.chomp.to_i
+    coin = vending_machine.bank.coins.find { |coin| coin.value == coin_value }.restock(amount_to_restock)
+    return
+  end
+
+  private
+
+  def enter_coins
+    puts 'enter coins'
+    loop do
+      coin = STDIN.gets.chomp
+      break if coin == ''
+      vending_machine.enter_coin(coin.to_i)
+    end
   end
 end
+
+VendingMachineController.new.start_vending_machine
--- spec/vending_machine_controller_spec.rb
+++ spec/vending_machine_controller_spec.rb
@@ -10,23 +10,65 @@ describe VendingMachineController do
 
   describe '#control_flow' do
     it 'directs to restock control flow if user inputs restock' do
+      allow(STDIN).to receive(:gets).and_return('restock')
       expect(vending_machine_controller).to receive(:restock_control_flow)
-      vending_machine_controller.control_flow('restock')
+      vending_machine_controller.control_flow
     end
 
     it 'directs to vending control flow if user inputs buy' do
+      allow(STDIN).to receive(:gets).and_return('buy')
       expect(vending_machine_controller).to receive(:vending_control_flow)
-      vending_machine_controller.control_flow('buy')
+      vending_machine_controller.control_flow
     end
 
     it 'normalises the input it gets to direct flow' do
+      allow(STDIN).to receive(:gets).and_return('  BUy  ')
       expect(vending_machine_controller).to receive(:vending_control_flow)
-      vending_machine_controller.control_flow('  BuY  ')
+      vending_machine_controller.control_flow
     end
 
     it 'exits if the user inputs exit' do
+      allow(STDIN).to receive(:gets).and_return('exit')
       expect(vending_machine_controller).to receive(:exit)
-      vending_machine_controller.control_flow('exit')
+      vending_machine_controller.control_flow
+    end
+  end
+
+  describe '#restock_control_flow' do
+    it 'directs to the products control flow if the user inputs product' do
+      allow(STDIN).to receive(:gets).and_return('products')
+      expect(vending_machine_controller).to receive(:products_restock_control_flow)
+      vending_machine_controller.restock_control_flow
+    end
+
+    it 'directs to the coins control flow if the user inputs coins' do
+      allow(STDIN).to receive(:gets).and_return('coins')
+      expect(vending_machine_controller).to receive(:coins_restock_control_flow)
+      vending_machine_controller.restock_control_flow
+    end
+  end
+
+  describe '#products_restock_control_flow' do
+    it 'send the restock message to products' do
+      allow(STDIN).to receive(:gets).and_return('1', '6')
+      expect_any_instance_of(Product).to receive(:restock).with(6)
+      vending_machine_controller.products_restock_control_flow
+    end
+  end
+
+  describe '#coins_restock_control_flow' do
+    it 'sends the restock message to coins' do
+      allow(STDIN).to receive(:gets).and_return('1', '20')
+      expect_any_instance_of(Money::Coin).to receive(:restock).with(20)
+      vending_machine_controller.coins_restock_control_flow
+    end
+  end
+
+  describe '#vending_control_flow' do
+    it 'allows you to buy a product' do
+      allow(STDIN).to receive(:gets).and_return('1', '20', '100', '', '1')
+      expect_any_instance_of(VendingMachine).to receive(:vend_item)
+      vending_machine_controller.vending_control_flow
     end
   end
 end
