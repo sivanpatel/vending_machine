8b7d5472ea0ff8e68ffe3c236c8c5a324eccc558
Sivan Patel <sivan.patel@googlemail.com>
2018-03-17 15:52:24 +0000

    basic user flow started

--- lib/vending_machine_controller.rb
+++ lib/vending_machine_controller.rb
@@ -9,4 +9,20 @@ class VendingMachineController
   def vending_machine
     @vending_machine
   end
+
+  def control_flow(input)
+    input = input.strip.downcase
+    return unless input == 'restock' || input == 'buy' || input == 'exit'
+    restock_control_flow if input == 'restock'
+    vending_control_flow if input == 'buy'
+    exit if input == 'exit'
+  end
+
+  def restock_control_flow
+
+  end
+
+  def vending_control_flow
+
+  end
 end
--- spec/vending_machine_controller_spec.rb
+++ spec/vending_machine_controller_spec.rb
@@ -7,4 +7,26 @@ describe VendingMachineController do
   it 'is initialized with an instance of VendingMachine' do
     expect(vending_machine_controller.vending_machine).to be_a VendingMachine
   end
+
+  describe '#control_flow' do
+    it 'directs to restock control flow if user inputs restock' do
+      expect(vending_machine_controller).to receive(:restock_control_flow)
+      vending_machine_controller.control_flow('restock')
+    end
+
+    it 'directs to vending control flow if user inputs buy' do
+      expect(vending_machine_controller).to receive(:vending_control_flow)
+      vending_machine_controller.control_flow('buy')
+    end
+
+    it 'normalises the input it gets to direct flow' do
+      expect(vending_machine_controller).to receive(:vending_control_flow)
+      vending_machine_controller.control_flow('  BuY  ')
+    end
+
+    it 'exits if the user inputs exit' do
+      expect(vending_machine_controller).to receive(:exit)
+      vending_machine_controller.control_flow('exit')
+    end
+  end
 end
