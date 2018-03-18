5d73034e67d1fcf540b951e8c148c0c8f63e5bc7
Sivan Patel <sivan.patel@googlemail.com>
2018-03-18 11:03:18 +0000

    test fix

--- lib/vending_machine_controller.rb
+++ lib/vending_machine_controller.rb
@@ -18,8 +18,8 @@ class VendingMachineController
   end
 
   def control_flow
-    puts 'inital control flow'
     input = STDIN.gets.chomp
+    input = input.strip.downcase
     return unless input == 'restock' || input == 'buy' || input == 'exit'
     restock_control_flow if input == 'restock'
     vending_control_flow if input == 'buy'
@@ -27,7 +27,6 @@ class VendingMachineController
   end
 
   def restock_control_flow
-    puts 'restock control flow'
     input = STDIN.gets.chomp
     input = input.strip.downcase
     return unless input == 'products' || input == 'coins'
@@ -36,9 +35,7 @@ class VendingMachineController
   end
 
   def vending_control_flow
-    puts 'vending control flow'
     enter_coins
-    puts 'now time to buy'
     product_code = STDIN.gets.chomp.to_i
     vending_machine.choose_product(product_code)
     vending_machine.vend_item
@@ -46,7 +43,6 @@ class VendingMachineController
   end
 
   def products_restock_control_flow
-    puts 'prod restock control flow'
     product_code = STDIN.gets.chomp.to_i - 1
     amount_to_restock = STDIN.gets.chomp.to_i
     vending_machine.stock.stocklist[product_code].restock(amount_to_restock)
@@ -54,7 +50,6 @@ class VendingMachineController
   end
 
   def coins_restock_control_flow
-    puts 'coin restock control flow'
     coin_value = STDIN.gets.chomp.to_i
     amount_to_restock = STDIN.gets.chomp.to_i
     coin = vending_machine.bank.coins.find { |coin| coin.value == coin_value }.restock(amount_to_restock)
@@ -64,14 +59,11 @@ class VendingMachineController
   private
 
   def enter_coins
-    puts 'enter coins'
     loop do
       coin = STDIN.gets.chomp
       break if coin == ''
       vending_machine.enter_coin(coin.to_i)
-      puts vending_machine.bank.sum_deposit(vending_machine.coins_entered)
     end
   end
 end
 
-VendingMachineController.new.start_vending_machine
